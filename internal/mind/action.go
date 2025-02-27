65e924be2378501ae6c2e480b4f9e4b919002cd6
farhoud.m7 <fymc5jspqhaqxwnt3z63myz6tv2kqls75xvtzgpbkh2e2unpl6vq>
2025-02-27 10:51:27 +0330

    add simple action excutor

---
+++ internal/mind/action.go
@@ -0,0 +1,36 @@
+package mind
+
+import (
+	"errors"
+
+	"github.com/go-vgo/robotgo"
+)
+
+func ExecAction(a Action, ai AnnotatedImage) error {
+	switch a.NextAction {
+	case "left_click":
+		x, y := ai.BoundedBox(a.BoxID)
+		robotgo.MoveSmooth(x, y)
+		robotgo.Click()
+	case "right_click":
+		x, y := ai.BoundedBox(a.BoxID)
+		robotgo.MoveSmooth(x, y)
+		robotgo.Click("right")
+	case "double_click":
+		x, y := ai.BoundedBox(a.BoxID)
+		robotgo.MoveSmooth(x, y)
+		robotgo.Click("left", true)
+	case "hover":
+		x, y := ai.BoundedBox(a.BoxID)
+		robotgo.MoveSmooth(x, y)
+	case "scroll_up":
+		robotgo.ScrollDir(10, "up")
+	case "scroll_down":
+		robotgo.ScrollDir(10, "down")
+	case "type":
+		robotgo.TypeStr(a.Value)
+	default:
+		return errors.New("action not found")
+	}
+	return nil
+}
